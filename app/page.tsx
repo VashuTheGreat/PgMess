ad31dec9d1f7f329932885389bb46dfb2c47a1ff
AnuragGupta181 <job4anurag11@gmail.com>
2025-05-13 16:35:05 +0530

    front

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
+'use client';
 
+import Link from 'next/link';
 
-const page = () => {
+export default function Home() {
   return (
-    <div>
-      I am a main page
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem pariatur molestias libero? Exercitationem ullam deleniti dicta, sit blanditiis dolores suscipit beatae at aliquid voluptates, rerum doloremque, ut quis assumenda dolore?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae ab nemo dolores, officiis, et adipisci amet repudiandae alias ipsam harum quaerat reprehenderit inventore culpa, eum eos dolorum laudantium perspiciatis.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque officiis laboriosam veniam magni quo repellendus eum suscipit natus velit libero hic et in, neque explicabo. Soluta quis beatae sapiente consequatur?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda odio autem hic facilis doloribus sint recusandae quae dicta, quasi voluptatem obcaecati temporibus officia aliquid accusamus ducimus quia, odit praesentium nisi!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti delectus accusantium, ratione cumque mollitia velit repellat sit enim harum voluptate ipsam nihil, laboriosam quibusdam deserunt. Reprehenderit consequatur totam nihil consequuntur.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque, labore quisquam quae excepturi a dignissimos quibusdam dolore eligendi et? Illo voluptates aspernatur tempora deserunt iste quis ipsam adipisci nobis ad?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias eveniet amet dicta dolores nobis eum, cumque culpa veritatis! Numquam earum recusandae voluptates. Quis at fugiat laborum culpa placeat quasi atque.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci expedita, aperiam obcaecati asperiores quod ipsam temporibus sapiente laudantium odit praesentium, vel perferendis consequatur magni voluptates libero ratione officiis. Optio, maxime?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, natus dolor ad ut neque vitae dolorum laudantium, eaque eligendi adipisci atque, deleniti hic laborum consequuntur labore vero porro voluptates exercitationem.
-      <div className='h-screen'></div>
-    </div>
-  )
-}
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 px-4 py-12">
+      <h1 className="text-4xl font-bold text-center mb-8">Welcome to MessMate</h1>
+      <p className="text-lg text-gray-600 text-center mb-12">
+        Choose the type of accommodation you are looking for
+      </p>
 
-export default page
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-md">
+        <Link
+          href="/properties?type=pg"
+          className="block bg-white rounded-lg shadow-md p-6 text-center hover:shadow-lg transition"
+        >
+          <h2 className="text-2xl font-semibold text-blue-600 mb-2">PG</h2>
+          <p className="text-gray-500">Browse available Paying Guest options near your college</p>
+        </Link>
+
+        <Link
+          href="/properties?type=mess"
+          className="block bg-white rounded-lg shadow-md p-6 text-center hover:shadow-lg transition"
+        >
+          <h2 className="text-2xl font-semibold text-green-600 mb-2">Mess</h2>
+          <p className="text-gray-500">Find mess facilities with food and meal options</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
